33d8b33eaeba3b16a0c0df72e586de7227ad1055
Brandon Ewoldt <bewoldt1@gmail.com>
2013-09-20 14:38:09 -0500

    adding social plugin

--- themes/annual-report/js/plugins.js
+++ themes/annual-report/js/plugins.js
@@ -1,3 +1,4 @@
 //@codekit-append "/vendor/jquery.isotope.min.js"
 //@codekit-append "/vendor/jquery-scrollto.js"
 //@codekit-append "/vendor/jquery.history.js"
+//@codekit-append "/vendor/socialcount.min.js"
