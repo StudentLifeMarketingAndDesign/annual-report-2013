a888f90a901fb96dc1d369af774928d2f943e0c3
Brandon Ewoldt <bewoldt1@gmail.com>
2013-10-18 13:22:15 -0500

    removing the pace script because it was annoying.
    Signed-off-by: Brandon Ewoldt <bewoldt1@gmail.com>

--- themes/annual-report/js/plugins.js
+++ themes/annual-report/js/plugins.js
@@ -2,4 +2,3 @@
 //@codekit-append "/plugins/jquery-scrollto.js"
 //@codekit-append "/plugins/jquery.history.js"
 //@codekit-append "/plugins/socialcount.min.js"
-//@codekit-append "/plugins/pace.min.js"
