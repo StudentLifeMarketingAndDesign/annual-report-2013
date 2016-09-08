d2a7ea98a0424461a1fba1595e428c90ec50d138
Dustin Quam <dustinquam@me.com>
2016-09-08 13:59:22 -0500

    allow script tags in tinymce

--- mysite/_config.php
+++ mysite/_config.php
@@ -9,7 +9,9 @@ $database = 'annualreport13';
 // Use _ss_environment.php file for configuration
 require_once("conf/ConfigureFromEnv.php");
 MySQLDatabase::set_connection_charset('utf8');
-
+HtmlEditorConfig::get('cms')->setOptions(array(
+	'extended_valid_elements' => "script[language|type|src]"
+));
 
 // Set the site locale
 i18n::set_locale('en_US');
