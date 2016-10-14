c30e6b3460666fcd88e64b66b53c668a5feea426
Dustin Quam <dustinquam@me.com>
2016-10-14 13:23:47 -0500

    added editor css and typekit

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,8 @@ module.exports = function(grunt) {
     sass: {
       dist: {
         files: {
-          '<%=globalConfig.themeDir %>/css/master.css' : '<%=globalConfig.themeDir %>/scss/master.scss'
+          '<%=globalConfig.themeDir %>/css/master.css' : '<%=globalConfig.themeDir %>/scss/master.scss',
+          '<%=globalConfig.themeDir %>/css/editor.css' : 'annual-report-project/scss/editor.scss'
         },                  // Target
         options: {              // Target options
           style: 'compressed'
